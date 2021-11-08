9267274fc63f6f36bb9847df206f4c9d61991981
Hamza Khalid <hamzer95@gmail.com>
2021-11-07 22:52:03 -0500

    added get an post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,52 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Workout = require("../models/workout.js");
 
-router.post("/api/transaction", ({ body }, res) => {
+//make new workout 
+router.post("/api/workouts", (req, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({ body }, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+//Get all previous workouts
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: { totalDuration: { $sum: '$exercises.duration' } }
+    }
+  ])
+
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
+
+//Get all previous workouts based on stats
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: { totalDuration: { $sum: '$exercises.duration' } }
+    }
+  ])
+
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
+
+
+
 
 module.exports = router;
